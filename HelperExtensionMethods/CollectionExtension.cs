35b20e39b60f75c8d01b20536662a88e2f0a6dc3
Nilesh Mitra <Nilesh.Mitra@gds.ey.com>
2020-06-27 23:09:36 +0530

    DistinctBy Helper Methods

---
+++ HelperExtensionMethods/CollectionExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HelperExtensionMethods
+{
+    public static class CollectionExtension
+    {
+        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector)
+        {
+            return source.DistinctBy(keySelector, null);
+        }
+        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector, IEqualityComparer<TKey> comparer)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (keySelector == null) throw new ArgumentNullException(nameof(keySelector));
+
+            return _(); /* artibitary name of local function => https://docs.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-7#local-functions */
+            IEnumerable<TSource> _()
+            {
+                var knownKeys = new HashSet<TKey>(comparer);
+                foreach (var element in source)
+                {
+                    if (knownKeys.Add(keySelector(element)))
+                        yield return element;
+                }
+            }
+        }
+    }
+}
---
+++ RunnerAppForClassLibs/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HelperExtensionMethods;
+
+namespace RunnerAppForClassLibs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<MyListItem> list = new List<MyListItem>() { 
+                new MyListItem()
+                {
+                    Name="Nilesh",
+                    Roll=1
+                },
+                new MyListItem()
+                {
+                    Name="Nilesh",
+                    Roll=2
+                },
+                new MyListItem()
+                {
+                    Name="Nilesh",
+                    Roll=2
+                },
+                new MyListItem()
+                {
+                    Name="Nikhil",
+                    Roll=1
+                },
+                new MyListItem()
+                {
+                    Name="Archana",
+                    Roll=1
+                },
+                new MyListItem()
+                {
+                    Name="Abhipsa",
+                    Roll=1
+                },
+                new MyListItem()
+                {
+                    Name="Abhipsa",
+                    Roll=2
+                },
+                new MyListItem()
+                {
+                    Name="Abhipsa",
+                    Roll=2
+                }
+            };
+
+            var distinctResult = list.DistinctBy(p => new { p.Roll, p.Name });
+            foreach (MyListItem item in distinctResult)
+            {
+                Console.WriteLine($"The name is {item.Name} and roll is {item.Roll.ToString()}");
+            }
+            Console.ReadLine();
+        }
+    }
+
+    public class MyListItem
+    {
+        public string Name { get; set; }
+        public int Roll { get; set; }
+    }
+}
