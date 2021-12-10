5d476716610e7d4236aab1d0bd2a33643023210f
Nilesh R Mitra <Nilesh.Mitra@gds.ey.com>
2021-12-10 20:47:02 +0530

    EventHubPublisherAndReciver

---
+++ EventHubProducerExp/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+
+namespace EventHubProducerExp
+{
+    class Program
+    {
+
+        // connection string to the Event Hubs namespace
+        private const string connectionString = "Endpoint=sb://nilesheventhubexp.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=gDJqP7AFf6ifRKka88zNgrQT8mwDkdp+w75KHW2Q2do=";
+
+        // name of the event hub
+        private const string eventHubName = "evthub1";
+
+        // number of events to be sent to the event hub
+        private const int numOfEvents = 3;
+
+        // The Event Hubs client types are safe to cache and use as a singleton for the lifetime
+        // of the application, which is best practice when events are being published or read regularly.
+        static EventHubProducerClient producerClient;
+
+
+        static async Task Main()
+        {
+            // Create a producer client that you can use to send events to an event hub
+            producerClient = new EventHubProducerClient(connectionString, eventHubName);
+            try
+            {
+                string userInput;
+                while (true)
+                {
+                    Console.WriteLine("Please provide the message input");
+                    userInput = Console.ReadLine();
+                    if (userInput.Trim().ToUpper().Equals("EXIT"))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.Write("Getting the partition count................");
+                        var partitions = await producerClient.GetPartitionIdsAsync();
+                        Console.WriteLine($"Total Numer  of Partition is {partitions.Length}");
+                        //
+                        Random choosingPartitionMock = new Random();
+                        SendEventOptions eventOptions = new SendEventOptions() { PartitionId = choosingPartitionMock.Next(0, partitions.Length).ToString() };
+                        //
+                        using EventDataBatch eventBatch = await producerClient.CreateBatchAsync();
+                        if (!eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes(userInput))))
+                        {
+                            // if it is too large for the batch
+                            throw new Exception($"{userInput} ------> is too large for the batch and cannot be sent.");
+                        }
+                        //
+                        await producerClient.SendAsync(eventBatch);
+                        Console.WriteLine("Events has been published.");
+                    }
+                }
+            }
+            finally
+            {
+                await producerClient.DisposeAsync();
+            }
+        }
+
+        private static string constructEventData()
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ EventHubReciver/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Consumer;
+using Azure.Messaging.EventHubs.Processor;
+
+namespace EventHubReciver
+{
+    class Program
+    {
+        private const string ehubNamespaceConnectionString = "Endpoint=sb://nilesheventhubexp.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=gDJqP7AFf6ifRKka88zNgrQT8mwDkdp+w75KHW2Q2do=";
+        private const string eventHubName = "evthub1";
+        private const string blobStorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=nileshevthubpocsa;AccountKey=5V01JzuMm9l9jUBP8rLIQfhJf4SWcOc3RQnEG4ZGOuqpA6xd1gsNr3tETH0EGhtHyHCzPlk4HYUmEQ1zz431ng==;EndpointSuffix=core.windows.net";
+        private const string blobContainerName = "checkpointblob";
+
+        static BlobContainerClient storageClient;
+        static EventProcessorClient processor;
+
+        static async Task Main()
+        {
+            // Read from the default consumer group: $Default
+            string consumerGroup = EventHubConsumerClient.DefaultConsumerGroupName;
+
+            // Create a blob container client that the event processor will use 
+            storageClient = new BlobContainerClient(blobStorageConnectionString, blobContainerName);
+
+            // Create an event processor client to process events in the event hub
+            processor = new EventProcessorClient(storageClient, consumerGroup, ehubNamespaceConnectionString, eventHubName);
+
+            // Register handlers for processing events and handling errors
+            processor.ProcessEventAsync += ProcessEventHandler;
+            processor.ProcessErrorAsync += ProcessErrorHandler;
+
+            // Start the processing
+            await processor.StartProcessingAsync();
+
+            // Wait for 30 seconds for the events to be processed
+            await Task.Delay(TimeSpan.FromSeconds(30));
+
+            // Stop the processing
+            await processor.StopProcessingAsync();
+        }
+        static async Task ProcessEventHandler(ProcessEventArgs eventArgs)
+        {
+            // Write the body of the event to the console window
+            Console.WriteLine("\tReceived event: {0}", Encoding.UTF8.GetString(eventArgs.Data.Body.ToArray()));
+
+            // Update checkpoint in the blob storage so that the app receives only new events the next time it's run
+            await eventArgs.UpdateCheckpointAsync(eventArgs.CancellationToken);
+        }
+
+        static Task ProcessErrorHandler(ProcessErrorEventArgs eventArgs)
+        {
+            // Write details about the error to the console window
+            Console.WriteLine($"\tPartition '{ eventArgs.PartitionId}': an unhandled exception was encountered. This was not expected to happen.");
+            Console.WriteLine(eventArgs.Exception.Message);
+            return Task.CompletedTask;
+        }
+    }
+}
