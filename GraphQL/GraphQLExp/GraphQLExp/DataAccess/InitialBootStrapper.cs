ff7d0175e8bf53c868dcfd4233f0333b9a00acc8
Nilesh Mitra <jmitrani@jci.com>
2022-05-12 11:11:53 +0530

    InitialSeedLogicComplete

--- GraphQL/GraphQLExp/GraphQLExp/DataAccess/InitialBootStrapper.cs
+++ GraphQL/GraphQLExp/GraphQLExp/DataAccess/InitialBootStrapper.cs
@@ -7,9 +7,15 @@ namespace GraphQLExp.DataAccess
     {
         public static void SeedInitialData(ApplicationDBContext dbContext)
         {
+            dbContext.Database.EnsureDeleted();
             dbContext.Database.EnsureCreated();
+            //dbContext.Database.Migrate();
+            //
             List<Address> AddressesMasterCopy = new List<Address>();
             List<Teacher> TeacherMasterCopy = new List<Teacher>();
+            List<Subject> SubjectMasterCopy = new List<Subject>();
+            List<Student> StudentMasterCopy = new List<Student>();
+            List<Score> ScoreMasterCopy = new List<Score>();
 
             #region Address
             var address1 = new Address() 
@@ -60,11 +66,69 @@ namespace GraphQLExp.DataAccess
                 dbContext.Database.ExecuteSqlRaw("SET IDENTITY_INSERT [dbo].[Teachers] OFF");
                 dbContext.Database.CloseConnection();
             }
-            
+
             #endregion
 
             #region Subject
+            for (int i = 1; i <= 5; i++)
+            {
+                var subject = new Subject()
+                {
+                    SubjectId = i,
+                    SubjectName = $"Subject {i.ToString()} ",
+                    AllotedTime = i*10,
+                    SubjectWeightage = i*5,
+                    AllotedTeachers = TeacherMasterCopy.Where(t => t.TeacherId <= i).ToList()
+                };
+                SubjectMasterCopy.Add(subject);
+                dbContext.Add(subject);
+                dbContext.Database.OpenConnection();
+                dbContext.Database.ExecuteSqlRaw("SET IDENTITY_INSERT [dbo].[Subjects] ON");
+                dbContext.SaveChanges();
+                dbContext.Database.ExecuteSqlRaw("SET IDENTITY_INSERT [dbo].[Subjects] OFF");
+                dbContext.Database.CloseConnection();
+            }
+            #endregion
 
+            #region Student
+            for (int i = 1; i <= 10; i++)
+            {
+                var student = new Student()
+                {
+                    StudentId = i,
+                    LastName = $"StudentLastName {i.ToString()} ",
+                    FirstName = $"StudentFirstName {i.ToString()} ",
+                    ContactAddress = AddressesMasterCopy[0],
+                    OptedSubjects = SubjectMasterCopy.Where(t => t.SubjectId <= i).ToList()
+                };
+                StudentMasterCopy.Add(student);
+                dbContext.Add(student);
+                dbContext.Database.OpenConnection();
+                dbContext.Database.ExecuteSqlRaw("SET IDENTITY_INSERT [dbo].[Students] ON");
+                dbContext.SaveChanges();
+                dbContext.Database.ExecuteSqlRaw("SET IDENTITY_INSERT [dbo].[Students] OFF");
+                dbContext.Database.CloseConnection();
+            }
+            #endregion
+
+            #region
+            for (int i = 1; i <= 10; i++)
+            {
+                var score = new Score()
+                {
+                    ScoreID = i,
+                    MarksForStudent = StudentMasterCopy.Where(t => t.StudentId == i).First(),
+                    MarksForSubject = (StudentMasterCopy.Where(t => t.StudentId == i).First()).OptedSubjects.Last(),
+                    MarksObtained = i * 15
+                };
+                ScoreMasterCopy.Add(score);
+                dbContext.Add(score);
+                dbContext.Database.OpenConnection();
+                dbContext.Database.ExecuteSqlRaw("SET IDENTITY_INSERT [dbo].[Scores] ON");
+                dbContext.SaveChanges();
+                dbContext.Database.ExecuteSqlRaw("SET IDENTITY_INSERT [dbo].[Scores] OFF");
+                dbContext.Database.CloseConnection();
+            }
             #endregion
         }
     }
