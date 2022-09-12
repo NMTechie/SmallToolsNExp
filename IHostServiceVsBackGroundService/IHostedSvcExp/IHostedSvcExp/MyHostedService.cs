ed6df19f95b6071f659ef49b5f3ee9a61aec4b48
Nilesh Mitra <jmitrani@jci.com>
2022-09-12 21:27:13 +0530

    IHostedService

---
+++ IHostServiceVsBackGroundService/IHostedSvcExp/IHostedSvcExp/MyHostedService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IHostedSvcExp
+{
+    public class MyHostedService : IHostedService
+    {
+        private readonly ILogger<MyHostedService> logger;
+
+        // inject a logger
+        public MyHostedService(ILogger<MyHostedService> logger)
+        {
+            this.logger = logger;
+        }
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Hosted service starting");
+
+            return Task.Factory.StartNew(async () =>
+            {
+                // loop until a cancalation is requested
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    logger.LogInformation("Hosted service executing - {0}", DateTime.Now);
+                    try
+                    {
+                        // wait for 3 seconds
+                        await Task.Delay(TimeSpan.FromSeconds(2), cancellationToken);
+                    }
+                    catch (OperationCanceledException) { }
+                }
+            }, cancellationToken);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Hosted service stopping");
+            return Task.CompletedTask;
+        }
+    }
+}
