901d0cef3d3adfd355de846495c960bcd129bb8f
Nilesh Mitra <jmitrani@jci.com>
2022-11-07 14:14:39 +0530

    Deployment covered

---
+++ IHostServiceVsBackGroundService/IHostedSvcExp/IHostedSvcExp/ActualScheduledOperation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace IHostedSvcExp
+{
+    /*
+     * In parallence of the OBBTimeseriesConsumerJobScheduler
+     */
+    public class ActualScheduledOperation
+    {
+        private readonly ILogger _logger;
+        public ActualScheduledOperation(EventHubConsumerHelper _evtHubHelper)
+        {
+
+        }
+    }
+}
---
+++ IHostServiceVsBackGroundService/IHostedSvcExp/IHostedSvcExp/EventHubConsumerHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+
+namespace IHostedSvcExp
+{
+    /*
+     * In parallance EventHubReceiver
+     */
+    public class EventHubConsumerHelper
+    {
+    }
+}
